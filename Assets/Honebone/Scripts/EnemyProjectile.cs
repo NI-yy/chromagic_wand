278bf731c58f322423c8a31bb50c563945108f75
honebone <ryou4402@icloud.com>
2023-09-20 17:51:46 +0900

    新enemy:spit plant, slimeの追加

---
+++ Assets/Honebone/Scripts/EnemyProjectile.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    EnemyProjectorData projectorData;
+    Transform tf;
+    Player player;
+    Transform playerTF;
+    Vector3 playerPos;
+    Vector3 playerPosDiff;
+    float projectileSpeed;
+    float followPlayerSpeed;
+
+    bool disabled;
+    public void Init(EnemyProjectorData data, Player p)
+    {
+        projectorData = data;
+        player = p;
+        playerTF=player.GetComponent<Transform>();
+        playerPos = playerTF.position;
+
+        projectileSpeed = Random.Range(projectorData.projectileSpeed_min, projectorData.projectileSpeed_max);
+        followPlayerSpeed=data.followPlayerSpeed;
+
+        tf = transform;
+
+
+        StartCoroutine(CountDown());
+    }
+    void FixedUpdate()
+    {
+        if (followPlayerSpeed > 0)//追尾弾
+        {
+            if (projectorData.followCurrentPlayer)//現在のプレイヤーの位置を追尾する場合は、プレイヤーの位置を常に更新
+            {
+                playerPos = playerTF.position;
+            }
+            playerPosDiff = (playerPos - tf.position);
+            Vector2 dis = playerPosDiff;
+            if (dis.magnitude < 0.5f ) { followPlayerSpeed = 0; }//プレイヤーの位置に到着したら追尾停止
+
+            float rot = (Mathf.Atan2(playerPosDiff.y, playerPosDiff.x) * Mathf.Rad2Deg) - tf.localEulerAngles.z - 90;
+            if (rot < -180) { rot += 360; }
+            tf.Rotate(0, 0, Mathf.Clamp(rot, followPlayerSpeed * -0.5f, followPlayerSpeed * 0.5f));
+        }
+
+        tf.Translate(Vector3.up * projectileSpeed / 33f);
+    }
+
+    void DestroyPJTL()
+    {
+        disabled = true;
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
+
+
+        Destroy(gameObject, 1f);
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(projectorData.projectileDuration);
+        if (!disabled) { DestroyPJTL(); }
+    }
+}
---
+++ Assets/Honebone/Scripts/HoneboneExtentions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HoneboneExtentions
+{
+    public static void Visualize(this Ray ray,float range)
+    {
+        Debug.DrawRay(ray.origin, ray.direction * range, Color.red);
+    }
+    public static Vector3 ToVector3(this Vector2 vector) { return new Vector3(vector.x, vector.y, 0); }
+    
+}
