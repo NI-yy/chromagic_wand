31c27eec7868d1f661b74e9ccedff26cb38e2338
malen <raspbell.caf2@gmail.com>
2023-10-21 15:58:22 +0900

    no message

---
+++ Assets/Malen/Scripts/circlePainter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circlePainter : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Camera subCamera;
+
+    [SerializeField]
+    private float scale;
+    
+    void Update()
+    {
+        Transform playerPosition = player.transform;
+        gameObject.transform.position = playerPosition.position;
+
+        subCamera.orthographicSize = scale;
+        transform.localScale = new Vector3(scale, scale, scale);
+
+    }
+
+}
