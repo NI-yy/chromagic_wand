1d2130649e777d1b2cbd78f4f42ac99195ab2c70
yyforXR <iy.nozomy@gmail.com>
2023-11-18 21:42:41 +0900

    ファイル名修正

--- Assets/y_y/Scripts/bulletController.cs
+++ Assets/y_y/Scripts/bulletController.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class bulletController : MonoBehaviour
 {
-    //[SerializeField]
-    //float moveSpeed = 10f;
-    private string color;
+    [SerializeField]
+    float moveSpeed = 10f;
+    string color;
     // Update is called once per frame
-    //void Update()
-    //{
-    //    transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
-    //}
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+    }
 
     public void SetBulletColor(string wand_color)
     {
-        Debug.Log(wand_color);
+        Debug.Log(("SetBulletColor wandcolor: " + wand_color, this.gameObject));
         color = wand_color;
-        Debug.Log(color);
+        Debug.Log(("SetBulletColor color: " + color, this.gameObject));
     }
 
     public string GetBulletColor()
     {
-        Debug.Log("GetBulletColor" + color);
+        Debug.Log(("GetBulletColor" + color, this.gameObject));
         return color;
     }
 }
